eaa372344cdcbdd1201c5d54937e3bdd0a09bcd7
talves <tony@3alves.com>
2018-01-13 13:09:32 -0800

    output

--- scripts/test-env.js
+++ scripts/test-env.js
@@ -4,7 +4,10 @@ console.log("process.env.context =", process.env.CONTEXT);
 const { spawn } = require('child_process');
 const child = spawn('pwd');
 
-child.on('exit', function (code, signal) {
-  console.log('child process exited with ' +
-              `code ${code} and signal ${signal}`);
+child.stdout.on('data', (data) => {
+  console.log(`child stdout:\n${data}`);
+});
+
+child.stderr.on('data', (data) => {
+  console.error(`child stderr:\n${data}`);
 });
