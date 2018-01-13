c24eab6ecc5bcc0c769f0815fbfde52bd4adee44
talves <tony@3alves.com>
2018-01-13 13:31:27 -0800

    test copy

--- scripts/test-env.js
+++ scripts/test-env.js
@@ -2,7 +2,18 @@
 console.log("process.env.context =", process.env.CONTEXT);
 
 const { spawn } = require('child_process');
-const child = spawn('ls', {
+
+let ls = spawn('ls', {
+  stdio: 'inherit',
+  shell: true
+});
+
+const child = spawn('cp -v content/* temp/', {
+  stdio: 'inherit',
+  shell: true
+});
+
+ls = spawn('ls', {
   stdio: 'inherit',
   shell: true
 });
