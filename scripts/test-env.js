6ea5466338426a39ec411b0a1f6eb1dfd09541f2
talves <tony@3alves.com>
2018-01-13 13:59:34 -0800

    error?

--- scripts/test-env.js
+++ scripts/test-env.js
@@ -1,9 +1,15 @@
 /* Test variable value */
 console.log("process.env.context =", process.env.CONTEXT);
 
+const command = ``
+
 const { spawn } = require('child_process');
 
-const child = spawn('ls && mkdir temp && cp -v content/* temp/ && ls', {
+const child = spawn('ls && mkdir temp && cp -v content/* temp/', {
   stdio: 'inherit',
   shell: true
+});
+
+child.stderr.on('data', (data) => {
+  console.error(`test-env stderr:\n${data}`);
 });
