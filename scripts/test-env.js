8a24859cac152a1698ae0f3d4f5381a610bdb1e2
talves <tony@3alves.com>
2018-01-13 13:21:45 -0800

    full spawn command (no params)

--- scripts/test-env.js
+++ scripts/test-env.js
@@ -2,7 +2,10 @@
 console.log("process.env.context =", process.env.CONTEXT);
 
 const { spawn } = require('child_process');
-const child = spawn('pwd');
+const child = spawn('ls', {
+  stdio: 'inherit',
+  shell: true
+});
 
 child.stdout.on('data', (data) => {
   console.log(`child stdout:\n${data}`);
