3d46e9a0222ed73abc68aa8a30664718fcefcc2b
talves <tony@3alves.com>
2018-01-13 14:31:05 -0800

    back to spawn

--- scripts/test-env.js
+++ scripts/test-env.js
@@ -3,16 +3,10 @@ const { spawn } = require('child_process');
 
 console.log("process.env.context =", process.env.CONTEXT);
 
-const command = 'cp';
-const args = ['-v', `example/assets/${ process.env.CONTEXT }/*`, 'example/'];
+const command = `cp -v example/assets/${ process.env.CONTEXT }/* example/`;
+const options = {
+  stdio: 'inherit',
+  shell: true
+};
 
-const child = spawn(command, args);
-
-child.stdout.on('data', (data) => {
-  console.log(`build-command stdout:\n${data}`);
-});
-
-child.on('error', (code, data) => {
-  console.log(`build-command error(${code}):\n${data}`);
-  throw 'build-command failed';
-});
+const child = spawn(command, options);
