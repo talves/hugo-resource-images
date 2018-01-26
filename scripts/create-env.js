8ea82624256f0e28df418c0605544cc466d55df5
talves <tony@3alves.com>
2018-01-26 08:30:19 -0800

    test create

---
+++ scripts/create-env.js
@@ -0,0 +1,26 @@
+const fs = require('fs')
+const path = require('path')
+/* Test variable value */
+const { spawn } = require('child_process');
+
+
+fs.writeFileSync(
+  path.join(__dirname, './.env'),
+  `API_KEY = ${process.env.API_KEY}\n`
+)
+
+console.log("process.env.context =", process.env.CONTEXT);
+
+const command = `more .env`;
+const options = {
+  stdio: 'inherit',
+  shell: true
+};
+
+const child = spawn(command, options);
+
+child.on('exit', function (code, signal) {
+  console.log('test-env process exited with ' +
+              `code ${code} and signal ${signal}`);
+  if (code !== 0) throw `error in command`;
+});
